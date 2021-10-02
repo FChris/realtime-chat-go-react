12e45b42f2206d7d6fb1a21aa75718dcb4d673a0
Christoph Frädrich <chris@treebug.de>
2021-10-02 11:14:03 +0200

    Improve frontend with chat history

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,25 +1,28 @@
 // api/index.js
 var socket = new WebSocket("ws://localhost:8080/ws")
 
-let connect = () => {
-    console.log("Attempting Connection...");
 
+//cb = callback to record messages
+let connect = cb => {
+    console.log("connecting");
+  
     socket.onopen = () => {
-        console.log("Successfully Connected");
-    }
-
-    socket.onmessage = msg => {
-        console.log(msg);
-    }
-
-    socket.onclose = event => {
-        console.log("Socket Closed Connection: ", event);
-    }
-
-    socket.onerror = error => {
-        console.log("Socket Error: ", error);
+      console.log("Successfully Connected");
     };
-};
+  
+    socket.onmessage = msg => {
+      console.log(msg);
+      cb(msg);
+    };
+  
+    socket.onclose = event => {
+      console.log("Socket Closed Connection: ", event);
+    };
+  
+    socket.onerror = error => {
+      console.log("Socket Error: ", error);
+    };
+  };
 
 let sendMsg = msg => {
     console.log("sending msg: ", msg);
