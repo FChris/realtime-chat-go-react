308969c11c10a3fea09ebeaf0e38552b60946c3b
Christoph Frädrich <chris@treebug.de>
2021-10-02 11:24:09 +0200

    Restructure Backend

--- backend/main.go
+++ backend/main.go
@@ -5,57 +5,26 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github.com/fchris/realtime-chat-go-react/pkg/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-func reader(conn *websocket.Conn) {
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			//error thrown, abort
-			log.Println(err)
-			return
-		}
-
-		fmt.Println(string(p))
-
-		err = conn.WriteMessage(messageType, p)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-	}
-
-}
-
 func serveWebSocket(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
+	ws, err := websocket.Upgrade(w, r)
 
-	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 	}
 
-	reader(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "SimpleServer")
-	})
-
 	http.HandleFunc("/ws", serveWebSocket)
 }
 
 func main() {
-	fmt.Println("Chat App v0.0.1")
+	fmt.Println("Distributed Chat App v0.0.1")
 	setupRoutes()
 	http.ListenAndServe(":8080", nil)
 
