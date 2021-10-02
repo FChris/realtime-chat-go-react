051b95967a385de5c057f5b75f61c7382b57de5c
Christoph Frädrich <chris@treebug.de>
2021-10-02 12:38:14 +0200

    Beautify frontend a bit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {connect, sendMsg} from "./api"
 
 import Header from './components/Header/Header';
 import ChatHistory from "./components/ChatHistory";
+import ChatInput from "./components/ChatInput";
 
 class App extends Component {
   constructor(props) {
@@ -23,19 +24,21 @@ class App extends Component {
     });
   }
 
-  send() {
-    console.log("hello");
-    sendMsg("Hello")
-  }
-
   render() {
     return (
       <div className="App">
         <Header />
         <ChatHistory chatHistory={this.state.chatHistory} />
-        <button onClick={this.send}>Hit</button>
+        <ChatInput send={this.send} />
       </div>
     );
+  }
+
+  send(event) {
+    if (event.keyCode === 13) {
+      sendMsg(event.target.value);
+      event.target.value = "";
+    }
   }
 }
 
